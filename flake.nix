a31d54b69c42c869483476b532ea68df08ffbdf1
Mason Mackaman <masondeanm@aol.com>
2021-03-02 20:49:26 -0500

    Have nix develop set up the entire environment

--- flake.nix
+++ flake.nix
@@ -1,18 +1,7 @@
-{
-# inputs =
-#     { shpaboinchkle =
-#         { url = "gitlab:fresheyeball/shpaboinchkle/mostly-miso";
-#           flake = false;
-#         };
-
-#       shpadoinkle =
-#         { url = "gitlab:fresheyeball/Shapadoinkle/hell-scape";
-#           flake = false;
-#         };
-#     };
+{ inputs.js-framework-benchmark.url = "github:ursi/js-framework-benchmark";
 
   outputs =
-    { self, nixpkgs, utils}:# , shpaboinchkle, shpadoinkle }:
+    { self, nixpkgs, js-framework-benchmark }:
       let
         system = "x86_64-linux";
 
@@ -20,8 +9,25 @@
           { inherit system;
             config = { allowUnfree = true; };
           };
-      in
-        { devShell.${system} = with pkgs;
+
+        inherit (nixpkgs) lib;
+
+        # use an absolute path from the js-framework-benchmarks dir
+        getNodeModules = path:
+          # the flake is used instead of the local directory so local changes don't make it have to rebuild
+          (import (js-framework-benchmark + path) { inherit pkgs system; }).shell.nodeDependencies
+            + /lib/node_modules;
+
+        nodeModules =
+          lib.mapAttrs
+            (_: path: getNodeModules path)
+            { main = /.;
+              webdriver = /webdriver-ts;
+              webdriverResults = /webdriver-ts-results;
+            };
+
+      in with pkgs;
+        { devShell.${system} =
             mkShell
               { buildInputs =
                   [ chromedriver
@@ -32,17 +38,49 @@
                   ];
 
                 shellHook =
-                  ''
-                  echo 'after running `npm install` inside `webdriver-ts`, run `patch-chromedriver` from the same directory'
+                  let
+                    help =
+                      lib.escapeShellArg
+                        ''
+                        run 'npm start' to start the server required to run the benchmakrs and view the results
 
-                  alias node2nix="node2nix -d -l package-lock.json"
+                        Commands:
+                          bench ([non-]keyed/<framework>)+     benchmark the given framework(s)
+                          results                              build the results table
+                      '';
+                  in
+                    ''
+                    rm -fr node_modules && ln -s ${nodeModules.main} node_modules
 
-                  patch-chromedriver () {
-                    mkdir node_modules/chromedriver/lib/chromedriver \
-                    && ln -fs $(which chromedriver) node_modules/chromedriver/lib/chromedriver \
-                    && echo '`chromedriver` patched successfully. You can now run `npm run bench ...`'
-                  }
-                  '';
+                    (
+                      cd webdriver-ts
+                      rm -fr node_modules && cp -r ${nodeModules.webdriver} node_modules
+                      chmod -R +w node_modules
+                      npm run build-prod
+                      cd node_modules/chromedriver/lib
+                      mkdir chromedriver
+                      ln -s ${chromedriver}/bin/chromedriver chromedriver
+                    )
+
+                    (
+                      cd webdriver-ts-results
+                      rm -fr node_modules && ln -s ${nodeModules.webdriverResults} node_modules
+                    )
+
+                    echo ${help}
+                    alias node2nix="node2nix -d -l package-lock.json"
+
+                    bench () (
+                      cd webdriver-ts
+                      npm run bench $@
+                    )
+
+                    results () (
+                      cd webdriver-ts
+                      npm run results
+                      echo -e "\nview table: http://localhost:8080/webdriver-ts-results/table.html"
+                    )
+                    '';
               };
         };
 }
