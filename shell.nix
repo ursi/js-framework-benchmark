a9ac408f185144ec1f86077bcade988061632c85
Mason Mackaman <masondeanm@aol.com>
2021-03-12 15:39:17 -0500

    Pin flake-compat

--- shell.nix
+++ shell.nix
@@ -1,4 +1,10 @@
 let
-  flake-compat = import (builtins.fetchGit "https://github.com/edolstra/flake-compat");
+  flake-compat =
+    import
+      (builtins.fetchGit
+        { url = "https://github.com/edolstra/flake-compat.git";
+          rev = "99f1c2157fba4bfe6211a321fd0ee43199025dbf";
+        }
+      );
 in
   (flake-compat { src = ./.; }).shellNix
